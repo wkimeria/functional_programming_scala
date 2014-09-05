a2bea47ce9080db4bf650fe7a5607635ec50ef59
Will Kimeria <william.kimeria@lookout.com>
2014-09-05 16:54:43 -0700

    Exercise 4 and 5 (uncurrying and compose)

---
+++ src/exercise_5/Compose.scala
@@ -0,0 +1,18 @@
+package exercise_5
+/**
+ * Found this link helpful
+ * https://twitter.github.io/scala_school/pattern-matching-and-functional-composition.html#composition
+ */
+object Compose {
+  def main(args: Array[String]): Unit = {
+    def f(s: String) = "f(" + s + ")"
+    def g(s: String) = "g(" + s + ")"
+    val composed = compose(f,g)
+    println(composed("Hello"))
+  }
+
+  /*EXERCISE 5: Implement the higher-order function that composes two functions.*/
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    (a: A) => f(g(a: A))
+  }
+}
